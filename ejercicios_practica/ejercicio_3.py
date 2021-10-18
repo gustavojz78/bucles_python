f82422fc4f8e22fb1d258d438ec7b1a4f8a7ccf2
Gustavo Adolfo Jaramillo Zambrano <59814986+gustavojz78@users.noreply.github.com>
2021-10-17 22:54:34 -0300

    Add files via upload
    Aun me falta pero ahí voy

--- ejercicios_practica/ejercicio_3.py
+++ ejercicios_practica/ejercicio_3.py
@@ -14,5 +14,7 @@
 # La sumatoria se deberá ir guardando en la variable "suma"
 numeros = [1, 5, -1, 6, 10, 2, -5]
 suma = 0   # Variable ya inicializada, la suma arranca en cero
-
-print("terminamos!, el resultado final almacenado en suma debe ser 18")
+for i in  numeros:
+    suma = i + suma
+    print("para el numero :",i , "la sumatoria da :", suma)
+print("terminamos!, el resultado final almacenado en suma debe ser 18 y es :", suma)
