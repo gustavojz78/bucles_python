15e503c002854af7db5ad39dcbe40e6353a80ffc
Gustavo Adolfo Jaramillo Zambrano <59814986+gustavojz78@users.noreply.github.com>
2021-10-17 22:52:53 -0300

    Add files via upload

--- ejercicios_practica/ejercicio_2.py
+++ ejercicios_practica/ejercicio_2.py
@@ -13,13 +13,24 @@
 # para imprimir en pantalla todos los colores
 colores = ['rojo', 'naranja', 'verde', 'azul']
 
+for i in range (4):
+   color=colores[i]
+   print("El color es : ", color)
+
+
 # Itere el "for" utilizando la lista como parámero
 # y utilizar como elemento del "for" cada color
 # for color ...
+
+for color in colores:
+    print("Les presento al color :",color)
+
 
 # Itere el "for" utilizando el tamaño de la lista
 # como parámetro y utilizar el índice para acceder a
 # los elementos de la lista
 # for i ...
-
+coloresL=len(colores)
+for i in range(coloresL):
+    print("Este es el color :", colores[i])
 print("terminamos!")
