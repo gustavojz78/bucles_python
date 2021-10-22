39b733056697e2ab75b1eda119f0467941422589
Gustavo Adolfo Jaramillo Zambrano <59814986+gustavojz78@users.noreply.github.com>
2021-10-21 22:41:50 -0300

    Add files via upload
    Notas

--- ejercicios_profunidzacion/profundizacion_3.py
+++ ejercicios_profunidzacion/profundizacion_3.py
@@ -10,7 +10,7 @@
 # de mucho más tiempo para abordar estos temas por su cuenta.
 # Requiere mayor tiempo de dedicación e investigación autodidacta.
 
-notas = [70, 82, -1, 65, 55, 67, 87, 92, -1]
+notas = [70, 82, -1, 65, 55, 67, 87, 92, -1, 56, 34, 91, 0]
 
 # IMPORTANTE: NO borrar los comentarios en VERDE o NARANJA
 
@@ -42,9 +42,14 @@ print("Mi organizador académico (#_#)")
 # Para calcular el promedio primero debe obtener la suma
 # de todas las notas, que irá almacenando en esta variable
 sumatoria = 0           # Ya le hemos inicializado en 0
-
+reprobado=0
+aprobado=0
 cantidad_notas = 0      # Aquí debe contar cuantas notas válidas encontró
 cantidad_ausentes = 0   # Aquí debe contar cuantos ausentes hubo
+cantidadA=0
+cantidadB=0
+cantidadC=0
+cantidadF=0
 
 # Realice aquí el bucle para recorrer todas las notas
 # y cacular la sumatoria
@@ -56,3 +61,40 @@ cantidad_ausentes = 0   # Aquí debe contar cuantos ausentes hubo
 # a calificación con letras, imprima en pantalla el resultado
 
 # Imprima en pantalla al cantidad de ausentes
+
+for i in notas:
+
+    if i<=0:
+        print("AUSENTE")
+        cantidad_ausentes+=1
+    elif i >= 0 and i <= 59 :
+        print ("Usted tiene una F y esta reprobado")
+        reprobado+=1
+        cantidad_notas+=1
+        sumatoria+=i
+        cantidadF+=1
+    elif i >=60 and i <=69 :
+        print ("Usted tiene una C")
+        aprobado+=1
+        cantidad_notas+=1
+        sumatoria+=i
+        cantidadC+=1
+    elif i >=70 and i <=89 :
+        print ("Usted tiene una B")
+        aprobado+=1
+        cantidad_notas+=1
+        sumatoria+=i
+        cantidadB+=1
+    elif i >=90 and i <=100 :
+        print ("Usted tiene una A")
+        aprobado+=1
+        cantidad_notas+=1
+        sumatoria+=i
+        cantidadA+=1
+print(" aprobaron", aprobado, " y reprobaron", reprobado, " alumnos, de un total de :", cantidad_notas, "alumnos presentes") 
+print("hubo ",cantidad_ausentes ," alumnos ausentes") 
+print("El promedio fue de ",sumatoria/cantidad_notas)
+print("Hubo ", cantidadA, "alumno(s) con A")
+print("Hubo ", cantidadB, "alumno(s) con B")
+print("Hubo ", cantidadC, "alumno(s) con C")
+print("Hubo ", cantidadF, "alumno(s) con F")
